dcc604bff67ef6d052db32a96628d9c8349c0e45
Haya10 <hayato.yabuta@sun-asterisk.com>
2025-08-30 14:15:43 +0900

    fix: avoid metadata sorting

--- webapp/spec/support/capybara.rb
+++ webapp/spec/support/capybara.rb
@@ -7,7 +7,7 @@ RSpec.configure do |config|
     driven_by :rack_test
   end
 
-  config.before(:each, :js, type: :system) do
+  config.before(:each, type: :system, js: true) do
     driven_by :selenium_chrome_headless
   end
 end
