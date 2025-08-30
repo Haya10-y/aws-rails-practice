dda29d2f9ee96967301e8247f8f9b3f777e371b5
Haya10 <hayato.yabuta@sun-asterisk.com>
2025-08-30 13:14:34 +0900

    feat: introduce rspec

---
+++ webapp/spec/system/homepage_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Application", type: :system do
+  it "renders the health check page" do
+    visit rails_health_check_path
+    expect(page).to have_http_status(200)
+  end
+
+  it "can access the sign up page" do
+    visit new_user_registration_path
+    expect(page).to have_content("Sign up")
+  end
+end
