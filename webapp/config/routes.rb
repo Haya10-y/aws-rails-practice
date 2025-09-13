0fd53aefab3389ba0d26f126eef41cd8375ff111
Haya10 <hayato.yabuta@sun-asterisk.com>
2025-09-13 16:38:09 +0900

    feat: add Sidekiq to perform async proccessing

--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Sidekiq Web interface
+  mount Sidekiq::Web => '/sidekiq'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
