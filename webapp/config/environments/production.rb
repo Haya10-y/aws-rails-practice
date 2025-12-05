ad72973499e5b7962a99834548642c2f20c775ff
Haya10 <hayato.yabuta@sun-asterisk.com>
2025-12-05 14:24:51 +0900

    fix: smtp settings for prod
    tmp.

--- webapp/config/environments/production.rb
+++ webapp/config/environments/production.rb
@@ -75,6 +75,21 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "webapp_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV.fetch('SMTP_HOST', 'localhost'),
+    port: 587,
+    user_name: ENV.fetch('SMTP_USERNAME', nil),
+    password: ENV.fetch('SMTP_PASSWORD', nil),
+    authentication: :login,
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = {
+    host: ENV.fetch('APP_HOST', 'example.com'),
+    protocol: 'https'
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
